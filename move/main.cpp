e568673018214b55d2b38844961b7ac1b36d8403
gsiri007 <216904534+gsiri007@users.noreply.github.com>
2026-04-13 12:50:03 +0530

    move

---
+++ move/main.cpp
@@ -0,0 +1,88 @@
+#include <raylib.h>
+#include <raymath.h>
+
+int main()
+{
+  const float windowWidth  { 1920 };
+  const float windowHeight { 1080 };
+
+  InitWindow(windowWidth, windowHeight, "move");
+
+  const float radius { 50 };
+  Vector2 position   { windowWidth / 2 - radius, windowHeight / 2 - radius };
+  Vector2 speed      { 200, 200 };
+  Vector2 direction  { 0.0f, 0.0f };
+
+
+  while (!WindowShouldClose())
+  {
+    auto dt { GetFrameTime() };
+
+
+    direction = { 0.0f, 0.0f }; // reset the direction each frame
+
+    // input
+
+    if (IsKeyDown(KEY_D))
+    {
+      direction.x += 1;
+    }
+
+    if (IsKeyDown(KEY_A))
+    {
+      direction.x -= 1;
+    }
+
+    if (IsKeyDown(KEY_W))
+    {
+      direction.y -= 1;
+    }
+
+    if (IsKeyDown(KEY_S))
+    {
+      direction.y += 1;
+    }
+
+    // update position
+
+    Vector2Normalize(direction);
+
+    position.x += direction.x * speed.x * dt;
+    position.y += direction.y * speed.y * dt;
+
+    // bound checking
+
+    if (position.x > windowWidth - radius)
+    {
+      position.x = windowWidth - radius;
+    }
+
+    if (position.x < radius)
+    {
+      position.x = radius;
+    }
+
+    if (position.y > windowHeight - radius)
+    {
+      position.y = windowHeight - radius;
+    }
+
+    if (position.y < radius)
+    {
+      position.y = radius;
+    }
+
+    // drawing
+
+    BeginDrawing();
+
+      DrawCircleV(position, radius, PINK);
+      ClearBackground(BLACK);
+
+    EndDrawing();
+  }
+
+  CloseWindow();
+
+  return 0;
+}
